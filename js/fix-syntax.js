3f678c5591479fbed5739bee7051a132a75635b7
Daniel Pinto <dpinto005@gmail.com>
2025-06-21 02:31:44 -0300

    Clean up temporary files and finalize deployment

--- js/fix-syntax.js
+++
@@ -1,9 +0,0 @@
-app.use(express.json({ 
-  limit: '922kb',
-  verify: (req, res, buf) => {
-    if (buf.length > 922 * 1024) {
-      throw new Error('Request entity too large');
-    }
-  }
-}));
-app.use(cors()); 
